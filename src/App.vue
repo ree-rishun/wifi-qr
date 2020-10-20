1bcd4595fa2a86112b17794e6b5b2a2bc9b5ed84
ree-rishun <ree0432@gmail.com>
2020-10-20 22:25:06 +0900

    完成では？

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Wi-Fi to QRcode</h1>
+
+    <div>
+      <input type="text" v-model="wifi.ssid" placeholder="SSID">
+    </div>
+
+    <div>
+      <input type="text" v-model="wifi.pass" placeholder="PASSWORD">
+    </div>
+
+    <div>
+      <select v-model="wifi.type">
+        <option value="WEP">WEP</option>
+        <option value="WPA">WPA/WPA2</option>
+      </select>
+    </div>
+
+    <button @click="generate">作成</button>
+
+    <div>
+      <vue-qrcode v-if="qrcode.targetText" :value="qrcode.targetText" :options="qrcode.option" tag="img"></vue-qrcode>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import VueQrcode from "@chenfengyuan/vue-qrcode";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            VueQrcode
+        },
+    data(){
+        return{
+            wifi:{
+                ssid: "",
+                pass: "",
+                type: "WPA",
+            },
+            qrcode:{
+                targetText: "",
+                option: "",
+            }
+        }
+    },
+        methods:{
+            generate(){ // QRコードを生成
+                if( this.wifi.ssid !== "" ){
+                    this.qrcode.targetText =
+                        "WIFI:T:" + this.wifi.type +
+                        ";S:" + this.wifi.ssid +
+                        ";P:" + this.wifi.pass + ";;";
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  input[type="text"], select{
+    margin-bottom: 10px;
+  }
 </style>
